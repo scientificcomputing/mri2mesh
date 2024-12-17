c96bcf05fba0ec70fd302c8d75680a95656d76d5
Henrik Finsberg <henrikfinsberg@hotmail.com>
2024-12-17 21:43:09 +0100

    Add docs

---
+++ docs/api.rst
@@ -0,0 +1,5 @@
+API Reference
+=============
+
+.. automodule:: mri2mesh
+    :members:
--- src/mri2mesh/surface/utils.py
+++ src/mri2mesh/surface/utils.py
@@ -4,7 +4,7 @@ import pyvista as pv
 def extract_surface(img, resolution=(1, 1, 1), origin=(0, 0, 0)):
     # img should be a binary 3D np.array
     grid = pv.ImageData(dimensions=img.shape, spacing=resolution, origin=origin)
-    mesh = grid.contour([0.5], img.flatten(order="F"), method="marching_cubes")
+    mesh = grid.contour(isosurfaces=[0.5], scalars=img.flatten(order="F"), method="marching_cubes")
     surf = mesh.extract_geometry()
     surf.clear_data()
     return surf
